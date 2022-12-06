92701d9a3cd2b61cebec44956b45324e6c7cf730
TerazKubi <kubacichowicz13@wp.pl>
2022-12-06 22:27:33 +0100

    add NoIdException and exception handling

--- src/main/java/pl/put/poznan/info/logic/BuildingInfo.java
+++ src/main/java/pl/put/poznan/info/logic/BuildingInfo.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.put.poznan.info.exceptions.NoIdException;
 import pl.put.poznan.info.model.Building;
 import pl.put.poznan.info.model.Level;
 import pl.put.poznan.info.model.Room;
@@ -15,22 +16,52 @@ import pl.put.poznan.info.rest.BuildingInfoController;
 public class BuildingInfo {
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfo.class);
 
-    public static Building createBuilding(JSONObject jsonObject) {
+    public static Building createBuilding(JSONObject jsonObject) throws NoIdException {
 
+        if(!jsonObject.has("id")) throw new NoIdException();
 
-        Building building = new Building((int) jsonObject.get("id"), (String) jsonObject.get("name"));
+        Building building;
+
+        //check if request body has name field
+        if (jsonObject.has("name")){
+            building = new Building(jsonObject.getInt("id"), jsonObject.getString("name"));
+        } else {
+            building = new Building(jsonObject.getInt("id"));
+        }
+
         logger.debug(building.getId() + " " + building.getName());
+
+        //if no level return empty building
+        if (!jsonObject.has("levels")) return building;
+
         JSONArray levels = jsonObject.getJSONArray("levels");
 
         for (Object lvl : levels){
 
             JSONObject jsonLevel = new JSONObject(lvl.toString());
-            Level level = new Level( (int) jsonLevel.get("id"), (String) jsonLevel.get("name"));
+
+            if (!jsonLevel.has("id")) throw new NoIdException();
+
+            Level level;
+            if (jsonLevel.has("name")) {
+                level = new Level( jsonLevel.getInt("id"), jsonLevel.getString("name"));
+            } else {
+                level = new Level( jsonLevel.getInt("id"));
+            }
+
+            //if level has no rooms continue
+            if (!jsonLevel.has("rooms")) continue;
 
             JSONArray rooms = jsonLevel.getJSONArray("rooms");
             for (Object r : rooms){
 
-                Room room = new Room(new JSONObject(r.toString()));
+                JSONObject jsonRoom = new JSONObject(r.toString());
+
+                //check if room has id
+                if(!jsonRoom.has("id")) throw new NoIdException();
+
+                //TODO add new exception for room fields
+                Room room = new Room(jsonRoom);
                 level.addRoom(room);
             }
 
--- src/main/java/pl/put/poznan/info/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/info/rest/BuildingInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
 
+
 import pl.put.poznan.info.logic.BuildingInfo;
 import pl.put.poznan.info.model.Building;
 import pl.put.poznan.info.model.Level;
@@ -25,7 +26,7 @@ public class BuildingInfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
 
-    //example request body
+//    example request body
 //    {
 //        "id": 1,
 //            "name": "BudynekTestowy",
@@ -75,135 +76,181 @@ public class BuildingInfoController {
     @RequestMapping(value = "/area/all", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getBuildingArea(@RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+
+            float areaAll = building.getArea();
+
+            JSONObject res = new JSONObject();
+            res.put("area", areaAll);
+            return res.toString();
+
+        } catch (Exception e) {
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
+        }
 
 
-        float areaAll = building.getArea();
-
-        JSONObject res = new JSONObject();
-        res.put("area", areaAll);
-        return res.toString();
     }
 
     @RequestMapping(value = "/cube/all", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getBuildingCube(@RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+            float cubeAll = building.getCube();
+
+            JSONObject res = new JSONObject();
+            res.put("cube", cubeAll);
+            return res.toString();
+        }
+        catch(Exception e){
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
+        }
 
 
-        float cubeAll = building.getCube();
 
-        JSONObject res = new JSONObject();
-        res.put("cube", cubeAll);
-        return res.toString();
+
     }
 
     @RequestMapping(value = "/light/all", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getBuildingLight(@RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+
+            float lightAll = building.getLightning();
+
+            JSONObject res = new JSONObject();
+            res.put("light", lightAll);
+            return res.toString();
+
+        } catch (Exception e) {
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
+        }
 
 
-        float lightAll = building.getLightning();
-
-        JSONObject res = new JSONObject();
-        res.put("light", lightAll);
-        return res.toString();
     }
 
     @RequestMapping(value = "/area/id/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getAreaById(@PathVariable int id, @RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
 
 
-        JSONObject res = new JSONObject();
+            JSONObject res = new JSONObject();
 
-        if (building.getId() == id) {
-            res.put("area", building.getArea());
-            res.put("id", id);
-        } else {
-            for (Level lvl : building.getLevels()){
-                if (lvl.getId() == id) {
-                    res.put("area", lvl.getArea());
-                    res.put("id", id);
-                    break;
-                }
-                for (Room room : lvl.getRooms()) {
-                    if (room.getId() == id) {
-                        res.put("area", room.getArea());
+            if (building.getId() == id) {
+                res.put("area", building.getArea());
+                res.put("id", id);
+            } else {
+                for (Level lvl : building.getLevels()){
+                    if (lvl.getId() == id) {
+                        res.put("area", lvl.getArea());
                         res.put("id", id);
                         break;
                     }
+                    for (Room room : lvl.getRooms()) {
+                        if (room.getId() == id) {
+                            res.put("area", room.getArea());
+                            res.put("id", id);
+                            break;
+                        }
+                    }
                 }
             }
+            //if ()
+            return res.toString();
+
+        } catch (Exception e) {
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
         }
-        //if ()
-        return res.toString();
     }
 
     @RequestMapping(value = "/cube/id/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCubeById(@PathVariable int id, @RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
 
+            JSONObject res = new JSONObject();
 
-        JSONObject res = new JSONObject();
-
-        if (building.getId() == id) {
-            res.put("cube", building.getCube());
-            res.put("id", id);
-        } else {
-            for (Level lvl : building.getLevels()){
-                if (lvl.getId() == id) {
-                    res.put("cube", lvl.getCube());
-                    res.put("id", id);
-                    break;
-                }
-                for (Room room : lvl.getRooms()) {
-                    if (room.getId() == id) {
-                        res.put("cube", room.getCube());
+            if (building.getId() == id) {
+                res.put("cube", building.getCube());
+                res.put("id", id);
+            } else {
+                for (Level lvl : building.getLevels()){
+                    if (lvl.getId() == id) {
+                        res.put("cube", lvl.getCube());
                         res.put("id", id);
                         break;
                     }
+                    for (Room room : lvl.getRooms()) {
+                        if (room.getId() == id) {
+                            res.put("cube", room.getCube());
+                            res.put("id", id);
+                            break;
+                        }
+                    }
                 }
             }
+            return res.toString();
+
+        } catch (Exception e) {
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
         }
-        //if ()
-        return res.toString();
+
+
     }
 
     @RequestMapping(value = "/light/id/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getLightById(@PathVariable int id, @RequestBody String reqBody) {
 
-        Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
+        try {
+            Building building = BuildingInfo.createBuilding(new JSONObject(reqBody));
 
 
-        JSONObject res = new JSONObject();
+            JSONObject res = new JSONObject();
 
-        if (building.getId() == id) {
-            res.put("light", building.getLightning());
-            res.put("id", id);
-        } else {
-            for (Level lvl : building.getLevels()){
-                if (lvl.getId() == id) {
-                    res.put("light", lvl.getLightning());
-                    res.put("id", id);
-                    break;
-                }
-                for (Room room : lvl.getRooms()) {
-                    if (room.getId() == id) {
-                        res.put("light", room.getLightning());
+            if (building.getId() == id) {
+                res.put("light", building.getLightning());
+                res.put("id", id);
+            } else {
+                for (Level lvl : building.getLevels()){
+                    if (lvl.getId() == id) {
+                        res.put("light", lvl.getLightning());
                         res.put("id", id);
                         break;
                     }
+                    for (Room room : lvl.getRooms()) {
+                        if (room.getId() == id) {
+                            res.put("light", room.getLightning());
+                            res.put("id", id);
+                            break;
+                        }
+                    }
                 }
             }
-        }
-        //if ()
-        return res.toString();
-    }
 
+            return res.toString();
+
+        } catch (Exception e) {
+            JSONObject res = new JSONObject();
+            res.put("message", e.toString());
+            return res.toString();
+        }
+    }
 }
 
 
