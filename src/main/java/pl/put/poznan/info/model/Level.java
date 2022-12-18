5613743f81f732eb7d5d6b6595dc1948e085638c
Pitarolas <matinoworolnik@gmail.com>
2022-12-18 21:12:02 +0100

    add javadoc auto documentation

--- src/main/java/pl/put/poznan/info/model/Level.java
+++ src/main/java/pl/put/poznan/info/model/Level.java
@@ -7,24 +7,46 @@ public class Level extends Location{
 
     private List<Room> rooms;
 
+    /**
+     *
+     * @param id level ID
+     */
     public Level(int id){
         super(id);
         this.rooms = new LinkedList<>();
     }
 
+    /**
+     *
+     * @param id level ID
+     * @param name level name
+     */
     public Level(int id, String name){
         super(id, name);
         this.rooms = new LinkedList<>();
     }
 
+
+    /**
+     * method adds the room to the list of rooms
+     * @param room room to add
+     */
     public void addRoom(Room room){
         this.rooms.add(room);
     }
 
+    /**
+     * the method returns a list of rooms
+     * @return list of rooms
+     */
     public List<Room> getRooms(){
         return this.rooms;
     }
 
+    /**
+     * the method sums up the area of all rooms on a level and returns the total area
+     * @return level Area
+     */
     @Override
     public float getArea() {
         float levelArea = 0;
@@ -34,6 +56,10 @@ public class Level extends Location{
         return levelArea;
     }
 
+    /**
+     * the method sums up the capacity of all rooms on a level and returns the total capacity
+     * @return capacity of the level
+     */
     @Override
     public float getCube() {
         float levelCube = 0;
@@ -43,6 +69,10 @@ public class Level extends Location{
         return levelCube;
     }
 
+    /**
+     * the method sums the lighting power of each room on a level and divides it by the total area of the level
+     * @return average light intensity in lux
+     */
     @Override
     public float getLightning() {
         float levelLight = 0;
@@ -52,6 +82,10 @@ public class Level extends Location{
         return levelLight / getArea();
     }
 
+    /**
+     * the method sums up the heat consumption of each room on a level and divides it by the total capacity of the level
+     * @return average heating energy consumption per m^3
+     */
     @Override
     public float getHeating() {
         float levelHeating = 0;
